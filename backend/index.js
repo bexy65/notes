4c6d8bc491dd5c6488553c32057fe243b202e3f1
bexy65 <naim.ertan98@gmail.com>
2026-08-03 10:19:37 +0300

    feat: #1 remove unnecessary files inital load bootstrap integration basic fetching on clientside

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const notesData = require("./notes");
 
 const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
